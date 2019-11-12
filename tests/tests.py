8cd5e92e1b21338e8b38f26cc346cb88c5ca4f0f
Peter Prescott <peter@prescott.ws>
2019-11-12 23:03:45 +0000

    move tests to own folder

--- tests/tests.py
+++ tests/tests.py
@@ -1,34 +1,30 @@
 """Run tests and document results."""
 
-import os.path
-import sys
 from datetime import datetime
 from selenium import webdriver  # for testing web app
 
-def test_flask_app(URL, page_title):
+def test_flask_app(page_location, page_title):
     """Tests that Flask App is running as expected.
-    
+
     Uses Selenium Webdriver to check Flask App is running as expected.
-    
+
     Args:
         URL (string): the address where the Flask App is running.
-        page_title (string): the title of the webpage, 
+        page_title (string): the title of the webpage,
             as it should be defined by <title> tags.
     """
     driver = webdriver.Chrome()
-    driver.get(URL)
-    if driver.title==page_title:
+    driver.get(page_location)
+    if driver.title == page_title:
         result = "\nTest Successful: Flask App running as expected."
     else:
         result = "\nError: Flask App not running as expected."
     print(result)
     return result
 
-if __name__=='__main__':
-    with open(os.path.join(sys.path[0], 'test_output', 'logs.txt'), 'a') as f:
-        now = datetime.now()
-        f.write("\n"+now.strftime("%d/%m/%Y %H:%M:%S"))
-        f.write(test_flask_app("localhost:5000","House Prices"))
+if __name__ == '__main__':
+    with open('logs.txt', 'a') as f:
+        NOW = datetime.now()
+        f.write("\n"+NOW.strftime("%d/%m/%Y %H:%M:%S"))
+        f.write(test_flask_app("localhost:5000", "House Prices"))
         f.write("\n")
-
-
